54b4f8cebc6f834acf9a615b7b25c0c3ba6f7523
ecruz95 <edwardfcruz@gmail.com>
2024-03-27 01:51:21 -0400

    Adjustments to classes. Add PcConfig for ComponentScan

--- spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/sterotype_annotation/PC.java
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/sterotype_annotation/PC.java
@@ -1,4 +1,29 @@
 package com.cydeo.sterotype_annotation;
 
+import com.cydeo.sterotype_annotation.casefactory.Case;
+import com.cydeo.sterotype_annotation.monitorfactory.Monitor;
+import com.cydeo.sterotype_annotation.motherboardfactory.Motherboard;
+
 public class PC {
+
+    private Case theCase;
+    private Monitor monitor;
+    private Motherboard motherboard;
+
+    public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
+        this.theCase = theCase;
+        this.monitor = monitor;
+        this.motherboard = motherboard;
+    }
+
+    public void powerUp() {
+        theCase.pressPowerButton();
+        drawLogo();
+    }
+
+    private void drawLogo() {
+        // Fancy graphics
+        monitor.drawPixelAt();
+    }
+
 }
--- spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/sterotype_annotation/monitorfactory/AcerMonitor.java
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/sterotype_annotation/monitorfactory/AcerMonitor.java
@@ -1,4 +1,16 @@
 package com.cydeo.sterotype_annotation.monitorfactory;
 
-public class AcerMonitor {
+import org.springframework.stereotype.Component;
+
+@Component
+public class AcerMonitor extends Monitor{
+
+    public AcerMonitor() {
+
+        super("25 inch beast","Acer",25);
+    }
+
+    public void drawPixelAt() {
+        System.out.println("Drawing pixel with Sony");
+    }
 }
