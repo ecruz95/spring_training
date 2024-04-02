d2e0c51d91c57706c5e9e96290b301883b2ebb93
ecruz95 <edwardfcruz@gmail.com>
2024-04-01 23:27:43 -0400

    Completion of DellCase and Dimensions classes as well as input to PcConfig class

--- spring-03-wiring/src/main/java/com/cydeo/casefactory/DellCase.java
+++ spring-03-wiring/src/main/java/com/cydeo/casefactory/DellCase.java
@@ -1,4 +1,12 @@
 package com.cydeo.casefactory;
 
-public class DellCase {
+public class DellCase extends Case{
+
+    public DellCase(String model, String manufacturer, String powerSupply, Dimensions dimensions){
+        super(model, manufacturer, powerSupply, dimensions);
+    }
+
+    public void pressPowerButton(){
+        System.out.println("Power button pressed");
+    }
 }
