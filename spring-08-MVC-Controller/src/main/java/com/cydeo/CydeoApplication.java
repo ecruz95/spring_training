b615b3ddd94bd2b2050c0bb2307726743c03d596
ecruz95 <edwardfcruz@gmail.com>
2024-04-15 23:45:05 -0400

    Create module "spring-08-MVC-Controller"

--- spring-08-MVC-Controller/src/main/java/com/cydeo/CydeoApplication.java
+++ spring-08-MVC-Controller/src/main/java/com/cydeo/CydeoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring08MvcControllerApplication {
+public class CydeoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Spring08MvcControllerApplication.class, args);
+        SpringApplication.run(CydeoApplication.class, args);
     }
 
 }
