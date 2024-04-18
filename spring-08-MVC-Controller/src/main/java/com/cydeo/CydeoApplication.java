5c91724eceac29baffad5a05840ee822cbfa3a30
ecruz95 <edwardfcruz@gmail.com>
2024-04-18 07:50:07 -0400

    Building paths to sites in local host

--- spring-08-MVC-Controller/src/main/java/com/cydeo/CydeoApplication.java
+++ spring-08-MVC-Controller/src/main/java/com/cydeo/CydeoApplication.java
@@ -2,8 +2,10 @@ package com.cydeo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration;
 
 @SpringBootApplication
+//@SpringBootApplication (exclude = {AopAutoConfiguration.class})
 public class CydeoApplication {
 
     public static void main(String[] args) {
